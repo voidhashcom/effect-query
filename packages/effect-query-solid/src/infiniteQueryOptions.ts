0f64776502f93207f23332f8dafca5c7c16f139f
Dominik Vít <slipiklp@gmail.com>
2025-10-28 16:12:43 +0100

    wip

---
+++ packages/effect-query-solid/src/infiniteQueryOptions.ts
@@ -0,0 +1,130 @@
+// import {
+//   type DefinedInitialDataInfiniteOptions,
+//   type InfiniteData,
+//   infiniteQueryOptions,
+//   type QueryFunctionContext,
+//   type SkipToken,
+//   type UndefinedInitialDataInfiniteOptions,
+//   type UseInfiniteQueryOptions,
+// } from "@tanstack/solid-query";
+// import { Cause, type Effect, Exit } from "effect";
+// import type { ManagedRuntime } from "effect/ManagedRuntime";
+// import { EffectQueryDefect, EffectQueryFailure } from "./errors";
+// import type { EffectQueryRunner } from "./runner";
+// import type { EffectQueryQueryKey } from "./types";
+
+// type EffectInfiniteQueryQueryFn<
+//   TFnResult,
+//   TFnErrorResult,
+//   TFnRequirements,
+//   TQueryKey extends EffectQueryQueryKey = EffectQueryQueryKey,
+//   TPageParam = never,
+// > = (
+//   context: QueryFunctionContext<TQueryKey, TPageParam>
+// ) => Effect.Effect<TFnResult, TFnErrorResult, TFnRequirements>;
+
+// type EffectInfiniteQueryUndefinedInitialDataOptions<
+//   TQueryFnData,
+//   TError,
+//   TData = InfiniteData<TQueryFnData, unknown>,
+//   TQueryKey extends EffectQueryQueryKey = EffectQueryQueryKey,
+// > = UndefinedInitialDataInfiniteOptions<TQueryFnData, TError, TData, TQueryKey>;
+
+// type EffectInfiniteQueryDefinedInitialDataOptions<
+//   TQueryFnData,
+//   TError,
+//   TData = InfiniteData<TQueryFnData, unknown>,
+//   TQueryKey extends EffectQueryQueryKey = EffectQueryQueryKey,
+// > = DefinedInitialDataInfiniteOptions<TQueryFnData, TError, TData, TQueryKey>;
+
+// export type EffectInfiniteQueryOptionsInput<
+//   TFnResult,
+//   TFnErrorResult,
+//   TFnRequirements,
+//   TQueryKey extends EffectQueryQueryKey = EffectQueryQueryKey,
+// > = Omit<
+//   | EffectInfiniteQueryUndefinedInitialDataOptions<
+//       TFnResult,
+//       TFnErrorResult,
+//       TFnResult,
+//       TQueryKey
+//     >
+//   | EffectInfiniteQueryDefinedInitialDataOptions<
+//       TFnResult,
+//       TFnErrorResult,
+//       TFnResult,
+//       TQueryKey
+//     >,
+//   "queryFn" | "queryKey"
+// > & {
+//   queryKey: EffectQueryQueryKey;
+//   queryFn: EffectInfiniteQueryQueryFn<
+//     TFnResult,
+//     TFnErrorResult,
+//     TFnRequirements,
+//     EffectQueryQueryKey
+//   >;
+// };
+
+// /**
+//  * @internal
+//  */
+// export function effectInfiniteQueryOptions<
+//   // biome-ignore lint/suspicious/noExplicitAny: generic
+//   TManagedRuntime extends ManagedRuntime<any, never>,
+//   TFnResult,
+//   TFnErrorResult extends { _tag: string },
+//   TFnRequirements,
+// >(
+//   inputOptions: EffectInfiniteQueryOptionsInput<
+//     TFnResult,
+//     TFnErrorResult,
+//     TFnRequirements
+//   >,
+//   context: {
+//     runner: EffectQueryRunner<TManagedRuntime>;
+//     signal?: AbortSignal;
+//   }
+// ) {
+//   const [spanName] = inputOptions.queryKey;
+
+//   const queryFn: Exclude<
+//     UseInfiniteQueryOptions<
+//       TFnResult,
+//       TFnErrorResult,
+//       InfiniteData<TFnResult, unknown>,
+//       EffectQueryQueryKey,
+//       unknown
+//     >["queryFn"],
+//     SkipToken | undefined
+//   > = async (queryFnContext) => {
+//     const effect = inputOptions.queryFn(queryFnContext);
+//     const result = await context.runner.run(effect, spanName, {
+//       signal: context.signal,
+//     });
+//     return Exit.match(result, {
+//       onSuccess: (value) => value,
+//       onFailure: (cause) => {
+//         if (cause._tag === "Fail") {
+//           const failure = cause.error;
+//           throw new EffectQueryFailure(Cause.pretty(cause), failure, cause);
+//         }
+//         throw new EffectQueryDefect(Cause.pretty(cause), cause);
+//       },
+//     });
+//   };
+
+//   // The as UseQueryOptions is a workaround to set the correct error type. React Query has no way to infer the error type from the Effect.
+//   return infiniteQueryOptions({
+//     ...inputOptions,
+//     queryKey: inputOptions.queryKey as EffectQueryQueryKey,
+//     queryFn,
+//   }) as UseInfiniteQueryOptions<
+//     TFnResult,
+//     [TFnErrorResult] extends [never]
+//       ? EffectQueryDefect<unknown>
+//       : EffectQueryFailure<TFnErrorResult> | EffectQueryDefect<unknown>,
+//     TFnResult,
+//     EffectQueryQueryKey
+//   >;
+// }
---
+++ packages/effect-query-solid/src/mutationOptions.ts
@@ -0,0 +1,97 @@
+import type {
+  MutationFunction,
+  SolidMutationOptions,
+  skipToken,
+  UseMutationOptions,
+} from "@tanstack/solid-query";
+import { Cause, type Effect, Exit } from "effect";
+import type { ManagedRuntime } from "effect/ManagedRuntime";
+import { EffectQueryDefect, EffectQueryFailure } from "./errors";
+import type { EffectQueryRunner } from "./runner";
+
+type EffectfulMutationFunction<
+  TFnResult,
+  TFnErrorResult,
+  TFnRequirements,
+  TVariables,
+> = (
+  variables: TVariables
+) => Effect.Effect<TFnResult, TFnErrorResult, TFnRequirements>;
+
+export type EffectQueryMutationOptionsInput<
+  TFnResult,
+  TFnErrorResult,
+  TFnRequirements,
+  TVariables,
+> = Omit<
+  UseMutationOptions<TFnResult, TFnErrorResult, TVariables>,
+  "mutationKey" | "mutationFn"
+> & {
+  mutationKey: string;
+  mutationFn:
+    | EffectfulMutationFunction<
+        TFnResult,
+        TFnErrorResult,
+        TFnRequirements,
+        TVariables
+      >
+    | typeof skipToken;
+};
+
+/**
+ * @internal
+ */
+export function effectQueryMutationOptions<
+  // biome-ignore lint/suspicious/noExplicitAny: generic
+  TManagedRuntime extends ManagedRuntime<any, never>,
+  TFnResult,
+  TFnErrorResult extends { _tag: string },
+  TFnRequirements,
+  TVariables,
+>(
+  inputOptions: EffectQueryMutationOptionsInput<
+    TFnResult,
+    TFnErrorResult,
+    TFnRequirements,
+    TVariables
+  >,
+  context: {
+    runner: EffectQueryRunner<TManagedRuntime>;
+  }
+) {
+  const spanName = inputOptions.mutationKey;
+  const mutationFn: MutationFunction<TFnResult, TVariables> = async (
+    variables: TVariables
+  ) => {
+    const effect = (
+      inputOptions.mutationFn as EffectfulMutationFunction<
+        TFnResult,
+        TFnErrorResult,
+        TFnRequirements,
+        TVariables
+      >
+    )(variables);
+    const result = await context.runner.run(effect, spanName);
+    return Exit.match(result, {
+      onSuccess: (value) => value,
+      onFailure: (cause) => {
+        if (cause._tag === "Fail") {
+          const failure = cause.error;
+          throw new EffectQueryFailure(Cause.pretty(cause), failure, cause);
+        }
+        throw new EffectQueryDefect(Cause.pretty(cause), cause);
+      },
+    });
+  };
+
+  return {
+    ...inputOptions,
+    mutationFn,
+  } as unknown as SolidMutationOptions<
+    TFnResult,
+    [TFnErrorResult] extends [never]
+      ? never
+      : EffectQueryFailure<TFnErrorResult> | EffectQueryDefect<unknown>,
+    TVariables
+  >;
+}
